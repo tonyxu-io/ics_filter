896c72c10d1be105e2d36e775d696095cfd345f3
Cxarli <10348289+Cxarli@users.noreply.github.com>
2020-08-30 21:19:21 +0200

    Better checking for Zoom

--- index.py
+++ index.py
@@ -2,31 +2,38 @@ import icalendar
 import toml
 import urllib.request
 
-# Read URL from config
+# Read URL from config file
 config = toml.load("config.toml")
 url = config['url']
 
 # Retrieve URL
-stream = urllib.request.urlopen(url).read()
+rawcal = urllib.request.urlopen(url).read()
 
-# Parse as feed
-feed = icalendar.Calendar.from_ical(stream)
+# Parse as calendar
+cal = icalendar.Calendar.from_ical(rawcal)
 
-# Make new feed
+# Make new calendar
 newcal = icalendar.Calendar()
 
 # Copy props
-for prop in feed:
-    newcal[prop] = feed[prop]
+for prop in cal:
+    newcal[prop] = cal[prop]
 
 # Update calname
 if 'X-WR-CALNAME' in newcal:
 	newcal['X-WR-CALNAME'] += " (filtered)"
 
 # Filter events
-for event in feed.walk('VEvent'):
-	if 'location' not in event or event['location'] != "Zoom Online Meeting":
-		newcal.add_component(event)
+for event in cal.walk('VEvent'):
+    
+    # Skip if Zoom
+    if 'DESCRIPTION' in event and "Zoom Meeting" in event['DESCRIPTION']:
+        continue
+    if 'LOCATION' in event and "Zoom Online Meeting" in event['LOCATION']:
+        continue
+
+    # Add event to new calendar
+    newcal.add_component(event)
 
 # Output new filtered calendar
 print(newcal.to_ical().decode('utf-8'))
